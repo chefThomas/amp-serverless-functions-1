88226f084b6c799ec1a24a3f08350a9f243f951f
chefthomas <thomcdixon@gmail.com>
2020-08-19 12:20:53 -0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./index.css";
+
+import "./App.css";
+
+import Amplify, { Auth } from "aws-amplify";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+
+import config from "./aws-exports";
+Amplify.configure(config);
 
 function App() {
+  const [user, updateUser] = useState(null);
+
+  // check for authenticated user on app mount. If user, set state
+  useEffect(() => {
+    Auth.currentAuthenticatedUser()
+      .then((user) => updateUser(user))
+      .catch((err) => console.log(err));
+  }, []);
+
+  let isAdmin = false;
+
+  if (user) {
+    // get token payload from
+    const {
+      signInUserSession: {
+        idToken: { payload },
+      },
+    } = user;
+    // inspect payload to see if user is part of admin group
+    console.log("payload: ", payload);
+    if (
+      payload["cognito:group"] &&
+      payload["cognito:group"].includes("Admin")
+    ) {
+      isAdmin = true;
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Hello World</h1>
+        {isAdmin && <p>Welcome, Admin</p>}
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
